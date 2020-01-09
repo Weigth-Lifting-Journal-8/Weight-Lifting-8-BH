4e980961eccc0baf2cc0b1c3d7e2bb0920bf4a23
Anthony Hill <hillan1152@gmail.com>
2020-01-09 13:54:45 -0500

    migration issue

--- routers/workout-router.spec.js
+++ routers/workout-router.spec.js
@@ -3,5 +3,11 @@ const db = require('../data/dbConfig.js');
 const workout = require('./workout-router.js');
 
 describe('WORKOUT-ROUTER', () => {
-    
+    describe("GET /:userId/all", function (){
+        it('Should Return Status 200 OK', async function() {
+            const res = await request(workout)
+                .get('/api/workouts/:userId/all')
+                    expect(res.status).toBe(200)
+        })
+    })
 });
